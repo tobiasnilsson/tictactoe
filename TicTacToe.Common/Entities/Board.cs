1118c37f9c5e3de8b46fd94b9cb109b5f57672a0
Tobias Nilsson <tobnil@kth.se>
2012-12-10 13:31:51 +0100

    removed whitespace

--- TicTacToe.Common/Entities/Board.cs
+++ TicTacToe.Common/Entities/Board.cs
@@ -5,7 +5,6 @@ namespace TicTacToe.Common.Entities
     public class Board
     {
         public List<DiscPosition> DiscsOnBoard { get; set; }
-
         public int BoundaryX { get; set; }
         public int BoundaryY { get; set; }
 
